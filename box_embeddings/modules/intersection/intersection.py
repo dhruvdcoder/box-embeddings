99197cf78eb6502a2e9b54433ec47affc2701066
dhruvdcoder <dhruvdplayer@gmail.com>
2021-02-02 00:12:07 -0500

    Added gumbel intersection pooling

--- box_embeddings/modules/intersection/intersection.py
+++ box_embeddings/modules/intersection/intersection.py
@@ -1,5 +1,5 @@
 from typing import List, Tuple, Union, Dict, Any, Optional
-from box_embeddings.parameterizations import BoxTensor
+from box_embeddings.parameterizations import BoxTensor, TBoxTensor
 import torch
 
 from box_embeddings.common.registrable import Registrable
@@ -8,7 +8,7 @@ from box_embeddings.common.registrable import Registrable
 class Intersection(torch.nn.Module, Registrable):
     """Base class for intersection Layer"""
 
-    def forward(self, left: BoxTensor, right: BoxTensor) -> BoxTensor:
+    def forward(self, left: TBoxTensor, right: TBoxTensor) -> BoxTensor:
         # broadcast if necessary
         # let the = case also be processed
 
---
+++ box_embeddings/modules/pooling/gumbel_intersection.py
@@ -0,0 +1,50 @@
+from box_embeddings.parameterizations.box_tensor import BoxTensor
+from .pooling import BoxPooler
+import torch
+
+
+def gumbel_intersection_pooler(
+    boxes: BoxTensor,
+    beta: float = 1e-4,
+    mask: torch.BoolTensor = None,
+    dim: int = 0,
+    keepdim: bool = False,
+) -> BoxTensor:
+    box_z = boxes.z
+    box_Z = boxes.Z
+
+    if mask is not None:
+        box_z[mask] -= float("inf")
+        box_Z[mask] += float("inf")
+    z = beta * torch.logsumexp(box_z / beta, dim=dim, keepdim=keepdim)
+    Z = -beta * torch.logsumexp(-box_Z / beta, dim=dim, keepdim=keepdim)
+
+    return BoxTensor.from_zZ(z, Z)
+
+
+@BoxPooler.register("hard-intersection")
+class HardIntersectionBoxPooler(BoxPooler):
+
+    """Pools a box tensor using hard intersection operation"""
+
+    def __init__(
+        self,
+        beta: float = 1e-4,
+        dim: int = 0,
+        keepdim: bool = False,
+    ):
+        super().__init__()  # type:ignore
+        self.beta = beta
+        self.dim = dim
+        self.keepdim = keepdim
+
+    def forward(  # type:ignore
+        self, box_tensor: BoxTensor, mask: torch.BoolTensor = None
+    ) -> BoxTensor:
+        return gumbel_intersection_pooler(
+            box_tensor,
+            beta=self.beta,
+            mask=mask,
+            dim=self.dim,
+            keepdim=self.keepdim,
+        )
